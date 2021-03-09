785d20d029b4cfc986e053c93755b4e4ab7ba689
heckfyxe <heckfyxe@gmail.com>
2021-03-09 23:27:19 +0300

    Added specs for User#author? method.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  let(:author_question) { create(:question) }
+  let(:author_answer) { create(:answer) }
+  let(:user) { create(:user, questions: [author_question], answers: [author_answer]) }
+
+  let(:question) { create(:question) }
+  let(:answer) { create(:answer) }
+
+  describe '#author?' do
+    it("returns true for user's question") { expect(user.author?(author_answer)).to be_truthy }
+    it("return true for user's answer") { expect(user.author?(author_question)).to be_truthy }
+
+    it("returns false for no user's question") { expect(user.author?(answer)).to be_falsey }
+    it("returns false for no user's answer") { expect(user.author?(answer)).to be_falsey }
+  end
 end
