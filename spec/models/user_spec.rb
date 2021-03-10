165dfd7c8eeadee2791b90be2b08c40efb1f60fe
heckfyxe <heckfyxe@gmail.com>
2021-03-11 00:44:11 +0300

    Refactored User model spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,17 +8,12 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :password }
 
   let(:author_question) { create(:question) }
-  let(:author_answer) { create(:answer) }
-  let(:user) { create(:user, questions: [author_question], answers: [author_answer]) }
+  let(:user) { create(:user, questions: [author_question]) }
 
   let(:question) { create(:question) }
-  let(:answer) { create(:answer) }
 
   describe '#author?' do
-    it("returns true for user's question") { expect(user.author?(author_answer)).to be_truthy }
-    it("return true for user's answer") { expect(user.author?(author_question)).to be_truthy }
-
-    it("returns false for no user's question") { expect(user.author?(answer)).to be_falsey }
-    it("returns false for no user's answer") { expect(user.author?(answer)).to be_falsey }
+    it { expect(user).to be_author(author_question) }
+    it { expect(user).to_not be_author(question) }
   end
 end
