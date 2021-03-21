0b38918879b3c5119643b8d32e8ccee98e244aa9
heckfyxe <heckfyxe@gmail.com>
2021-03-21 18:20:48 +0300

    User can attach files to edited question.

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -9,19 +9,20 @@ feature 'User can edit his question', %q{
   given!(:user) { create(:user) }
   given!(:question) { create(:question, author: user) }
 
-  scenario 'Unauthenticated cannot edit answer' do
+  scenario 'Unauthenticated cannot edit question' do
     visit questions_path
 
     expect(page).to_not have_link 'Edit'
   end
 
-  describe 'Authenticated user', js: true do
-    scenario 'edits his answer' do
+  describe 'Author', js: true do
+    background do
       sign_in user
       visit questions_path
-
       click_on 'Edit'
+    end
 
+    scenario 'edits his question' do
       fill_in 'Title', with: 'updated title'
       fill_in 'Body', with: 'updated body'
       click_on 'Update'
@@ -30,12 +31,7 @@ feature 'User can edit his question', %q{
       expect(page).to have_content 'updated body'
     end
 
-    scenario 'edits his answer with errors' do
-      sign_in user
-      visit questions_path
-
-      click_on 'Edit'
-
+    scenario 'edits his question with errors' do
       fill_in 'Title', with: ''
       fill_in 'Body', with: ''
       click_on 'Update'
@@ -43,11 +39,26 @@ feature 'User can edit his question', %q{
       expect(page).to have_content "Body can't be blank"
     end
 
-    scenario "tries to edit other user's question" do
-      user = create(:user)
-      sign_in user
-      visit questions_path
+    scenario 'edits his question and attach files' do
+      fill_in 'Title', with: 'updated title'
+      fill_in 'Body', with: 'updated body'
+      attach_file 'Files', %W[#{Rails.root}/spec/rails_helper.rb #{Rails.root}/spec/spec_helper.rb]
+      click_on 'Update'
 
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
+  end
+
+  context 'No author' do
+    let!(:no_author) { create(:user) }
+
+    background do
+      sign_in no_author
+      visit questions_path
+    end
+
+    scenario "tries to edit other user's question" do
       expect(page).to_not have_content 'Edit'
     end
   end
