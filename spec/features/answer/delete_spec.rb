52b88a03077608f8a59648119b67205260112e77
heckfyxe <heckfyxe@gmail.com>
2021-03-22 21:07:41 +0300

    User can delete answer's attachments.

--- spec/features/answer/delete_spec.rb
+++ spec/features/answer/delete_spec.rb
@@ -5,24 +5,41 @@ feature 'User can delete the answer', %q{
   I'd like to be able to delete the answer
 } do
   let(:user) { create(:user) }
-  let(:question) { create(:question) }
-  let!(:answer) { create(:answer, question: question) }
+  let(:question) { create(:question, author: user) }
+  let!(:answer) { create(:answer, :with_attachment, author: user, question: question) }
 
-  describe 'Authenticated user' do
+  describe 'Author', js: true do
     background { sign_in(user) }
 
-    scenario 'Author deletes the answer', js: true do
-      answer.author = user
-      answer.save!
-
+    scenario 'deletes the answer' do
       visit question_path(question)
-      click_on 'Delete'
+      find("a[href='#{answer_path(answer)}']").click
 
       expect(page).to_not have_content answer.body
     end
 
+    scenario 'deletes the attachments' do
+      visit question_path(question)
+      within '.attachments' do
+        click_on 'Delete'
+      end
+
+      expect(page).to_not have_content answer.files.first.filename.to_s
+    end
+  end
+
+  describe 'No author' do
+    let(:no_author) { create(:user) }
+    background { sign_in(no_author) }
+
     scenario 'Not author tries to delete the answer' do
       visit question_path(question)
+      expect(page).to_not have_content 'Delete'
+    end
+
+    scenario 'tries to delete the attachments' do
+      visit question_path(question)
+
       expect(page).to_not have_content 'Delete'
     end
   end
