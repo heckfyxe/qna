f77bb08b9b15aeeba2063c4ba3bf0d2c9c24f720
heckfyxe <heckfyxe@gmail.com>
2021-03-23 20:05:17 +0300

    User can add links to new answer.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,11 +4,14 @@ class Answer < ApplicationRecord
 
   belongs_to :question
   belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :links, dependent: :destroy, as: :linkable
 
   has_many_attached :files
 
   validates :body, presence: true
   validate :one_the_best_in_question
+
+  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   def mark_as_the_best
     transaction do
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
   it { should belong_to(:author).class_name('User').with_foreign_key('author_id') }
+  it { should have_many(:links).dependent(:destroy) }
 
   it { should validate_presence_of :body }
   it 'validates one the best answer for question' do
@@ -16,4 +17,6 @@ RSpec.describe Answer, type: :model do
   it 'have many attached files' do
     expect(Question.new.files).to be_an_instance_of ActiveStorage::Attached::Many
   end
+
+  it { should accept_nested_attributes_for :links }
 end
