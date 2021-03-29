7af838a679cf57859e4379e4ea85527412fb556b
heckfyxe <heckfyxe@gmail.com>
2021-03-29 15:04:10 +0300

    Links to Github gists are displayed as gists.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def github_gist?(url)
+    url.match /https?:\/\/gist.github.com\/[a-zA-Z0-9]*\/[a-z0-9]*/
+  end
+
+  def github_gist_id(url)
+    url.split('/').last if github_gist?(url)
+  end
 end
--- spec/features/answer/add_links_spec.rb
+++ spec/features/answer/add_links_spec.rb
@@ -7,21 +7,51 @@ feature 'User can add links to answer', %q{
 } do
   given(:user) { create(:user) }
   given(:question) { create(:question) }
+  given(:google_url) { 'https://google.com' }
   given(:gist_url) { 'https://gist.github.com/vkurennov/743f9367caa1039874af5a2244e1b44c' }
 
-  scenario 'User adds link when give an answer', js: true do
+  background do
     sign_in(user)
     visit question_path(question)
-
     fill_in 'Answer', with: 'My answer'
+  end
 
+  scenario 'User adds link when give an answer', js: true do
+    fill_in 'Link name', with: 'Google'
+    fill_in 'Url', with: google_url
+
+    click_on 'To answer'
+
+    within '.answers' do
+      expect(page).to have_link 'Google', href: google_url
+    end
+  end
+
+  scenario 'User adds several links when answers', js: true do
+    all('.nested-fields').last.tap do |f|
+      f.fill_in 'Link name', with: 'My gist'
+      f.fill_in 'Url', with: gist_url
+    end
+
+    click_on 'Add link'
+
+    all('.nested-fields').last.tap do |l|
+      l.fill_in 'Link name', with: 'Google'
+      l.fill_in 'Url', with: google_url
+    end
+
+    click_on 'To answer'
+
+    expect(page).to have_content 'hosted with ❤ by GitHub'
+    expect(page).to have_link 'Google', href: google_url
+  end
+
+  scenario 'User adds link to github gist', js: true do
     fill_in 'Link name', with: 'My gist'
     fill_in 'Url', with: gist_url
 
     click_on 'To answer'
 
-    within '.answers' do
-      expect(page).to have_link 'My gist', href: gist_url
-    end
+    expect(page).to have_content 'hosted with ❤ by GitHub'
   end
 end
