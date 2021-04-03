a025319570cfb24fcd345635d68f411fad2fe924
heckfyxe <heckfyxe@gmail.com>
2021-04-03 22:34:21 +0300

    Added Ajax JSON for answer creation.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,7 +4,14 @@ class AnswersController < ApplicationController
   def create
     @answer = question.answers.build(answer_params)
     @answer.author = current_user
-    @answer.save
+
+    respond_to do |format|
+      if @answer.save
+        format.json { render json: @answer }
+      else
+        format.json { render json: @answer.errors.full_messages, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
--- app/javascript/packs/answers.js
+++ app/javascript/packs/answers.js
@@ -7,4 +7,19 @@ document.addEventListener('turbolinks:load', () => {
         let answerId = e.target.dataset.answerId
         document.querySelector('form#edit-answer-' + answerId).classList.remove('d-none')
     })
+
+    document.querySelector('form.new-answer').addEventListener('ajax:success', (e) => {
+        var answer = e.detail[0]
+
+        document.querySelector('.answer-errors').innerHTML = ''
+        document.querySelector('.answers').innerHTML += '<p>' + answer.body + '</p>'
+    })
+
+    document.querySelector('form.new-answer').addEventListener('ajax:error', (e) => {
+        var errors = e.detail[0]
+        document.querySelector('.answer-errors').innerHTML = ''
+        errors.forEach((val) => {
+            document.querySelector('.answer-errors').innerHTML += '<p>' + val + '</p>'
+        })
+    })
 })
