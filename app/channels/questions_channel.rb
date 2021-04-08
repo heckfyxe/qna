dfbedad6aacc3e29ec8b8e83024e8776d09c5aa6
heckfyxe <heckfyxe@gmail.com>
2021-04-09 00:14:44 +0300

    App shows new answers in realtime.

--- app/channels/questions_channel.rb
+++ app/channels/questions_channel.rb
@@ -1,22 +1,4 @@
 class QuestionsChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from QuestionsChannel.stream_name(current_user)
-  end
-
-  def unsubscribed
-    stop_all_streams
-  end
-
-  def self.broadcast_to_user(user, &message)
-    ActionCable.server.broadcast stream_name(user), message.call(user)
-  end
-
-  def self.broadcast_except_user(user, &message)
-    ApplicationCable::ConnectedUsers.except(user).each do |user|
-      ActionCable.server.broadcast stream_name(user), message.call(user)
-    end
-  end
-
   def self.stream_name(user)
     "questions_for_user_#{user&.id}"
   end
--- app/javascript/packs/answers.js
+++ app/javascript/packs/answers.js
@@ -1,27 +1,36 @@
+import {cable} from "./application";
+
 document.addEventListener('turbolinks:load', () => {
-    document.querySelector('.answers').addEventListener('click', (e) => {
-        if (!e.target.matches('.edit-answer-link'))
-            return
+    var answers = document.querySelector('.answers')
+    if (answers) {
+        answers.addEventListener('click', (e) => {
+            if (!e.target.matches('.edit-answer-link'))
+                return
 
-        e.preventDefault()
-        let answerId = e.target.dataset.answerId
-        document.querySelector('form#edit-answer-' + answerId).classList.remove('d-none')
-    })
-
-    document.querySelector('form.new-answer').addEventListener('ajax:success', (e) => {
-        var answer = e.detail[0]
-
-        document.querySelector('.answer-errors').innerHTML = ''
-        document.querySelector('.answers').innerHTML += '<p>' + answer.body + '</p>'
-    })
-
-    document.querySelector('form.new-answer').addEventListener('ajax:error', (e) => {
-        var errors = e.detail[0]
-        document.querySelector('.answer-errors').innerHTML = ''
-        errors.forEach((val) => {
-            document.querySelector('.answer-errors').innerHTML += '<p>' + val + '</p>'
+            e.preventDefault()
+            let answerId = e.target.dataset.answerId
+            document.querySelector('form#edit-answer-' + answerId).classList.remove('d-none')
         })
-    })
+    }
+
+    var formNewAnswer = document.querySelector('form.new-answer')
+
+    if (formNewAnswer) {
+        formNewAnswer.addEventListener('ajax:success', (e) => {
+            var answer = e.detail[0]
+
+            document.querySelector('.answer-errors').innerHTML = ''
+            document.querySelector('.answers').innerHTML += '<p>' + answer.body + '</p>'
+        })
+
+        formNewAnswer.addEventListener('ajax:error', (e) => {
+            var errors = e.detail[0]
+            document.querySelector('.answer-errors').innerHTML = ''
+            errors.forEach((val) => {
+                document.querySelector('.answer-errors').innerHTML += '<p>' + val + '</p>'
+            })
+        })
+    }
 
     document.querySelectorAll('.vote-answer').forEach((element) => {
         element.addEventListener('ajax:success', (e) => {
@@ -32,8 +41,8 @@ document.addEventListener('turbolinks:load', () => {
 
             document.querySelector('#answer-rating-' + resourceId).innerHTML = 'Rating: ' + rating
 
-            vote_up = document.querySelectorAll('.vote-answer[data-id="' + resourceId + '"]')[0]
-            vote_down = document.querySelectorAll('.vote-answer[data-id="' + resourceId + '"]')[1]
+            var vote_up = document.querySelectorAll('.vote-answer[data-id="' + resourceId + '"]')[0]
+            var vote_down = document.querySelectorAll('.vote-answer[data-id="' + resourceId + '"]')[1]
             if (my_vote < 1) {
                 vote_up.classList.remove('invisible')
             }
@@ -48,4 +57,16 @@ document.addEventListener('turbolinks:load', () => {
             }
         })
     })
+
+    if (answers) {
+        cable.subscriptions.create('AnswersChannel', {
+            connected() {
+                console.log('Connected!')
+            },
+            received(data) {
+                console.log(data)
+                answers.innerHTML += data
+            }
+        })
+    }
 })
